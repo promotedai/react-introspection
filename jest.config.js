fd16e9b55a08aa3d8131f726f335cd7fbc1999e0
Yuna Wang <yuna@promoted.ai>
2022-09-08 09:35:47 -0700

    Add cell components

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {},
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 40,
+      functions: 40,
+      lines: 65,
+      statements: 65,
+    },
+  },
+  coverageReporters: ['text'],
+};
